7a348ebb653f21739235385b915169318f40fe3c
hendrixgotcodes <asare11samuel@gamil.com>
2020-08-22 03:41:48 +0200

    Added js to project. Added functionality to checkbox

---
+++ index.js
@@ -0,0 +1,10 @@
+// Getting DOM elements
+const checkBox = document.getElementById("checkbox");
+const body = document.querySelector("body");
+
+//Adding event listener to checkbox
+checkBox.addEventListener("change",()=>{
+
+    // Switching between a dark class of the body depending on checkbox's state
+    body.classList.toggle("body--dark");
+})
